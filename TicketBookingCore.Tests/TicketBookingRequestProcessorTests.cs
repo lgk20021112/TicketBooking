860332f45b24d05da78f167491f20c69f6c6df7f
lukasg2002 <143589671+lukasg2002@users.noreply.github.com>
2025-04-03 23:04:13 +0200

    Add project files.

---
+++ TicketBookingCore.Tests/TicketBookingRequestProcessorTests.cs
@@ -0,0 +1,85 @@
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Client;
+using Moq;
+
+namespace TicketBookingCore.Tests
+{
+    public class TicketBookingRequestProcessorTests
+    {
+        private readonly TicketBookingRequest _request;
+        private readonly Mock<ITicketBookingRepository> _ticketBookingRepositoryMock;
+        private readonly TicketBookingRequestProcessor _processor;
+        public TicketBookingRequestProcessorTests()
+        {
+            _request = new TicketBookingRequest
+            {
+                FirstName = "Namn",
+                LastName = "Efternamn",
+                Email = "email@gmail.com"
+            };
+            _ticketBookingRepositoryMock = new Mock<ITicketBookingRepository>();
+            _processor = new TicketBookingRequestProcessor(_ticketBookingRepositoryMock.Object);
+        }
+
+        [Fact]
+        public void ShouldReturnTicketBookningResultWithRequestValues()
+        {
+            // Arrange
+
+            var request = new TicketBookingRequest
+            {
+                FirstName = "Namn",
+                LastName = "Efternamn",
+                Email = "email@gmail.com"
+            };
+
+            // Act
+            TicketBookingResponse response = _processor.Book(request);
+
+            // Assert
+            Assert.NotNull(response);
+            Assert.Equal(request.FirstName, response.FirstName);
+            Assert.Equal(request.LastName, response.LastName);
+            Assert.Equal(request.Email, response.Email);
+        }
+        [Fact]
+        public void ShouldThrowExceptionIfRequestIsNull()
+        {
+            // Act
+            var exception = Assert.Throws<ArgumentNullException>(() => _processor.Book(null));
+
+            //Assert
+            Assert.Equal("request", exception.ParamName);
+        }
+
+        /// <summary>
+        /// This test will fail because the method is not implemented yet.
+        /// </summary>
+        [Fact]
+        public void ShouldSaveToDatabase()
+        {
+            // Arrange
+            TicketBooking savedTicketBooking = null;
+
+            // Setup the Save method to capture the saved ticket booking
+            _ticketBookingRepositoryMock.Setup(x => x.Save(It.IsAny<TicketBooking>()))
+            .Callback<TicketBooking>((ticketBooking) =>
+            {
+                savedTicketBooking = ticketBooking;
+            });
+
+
+            // Act
+            _processor.Book(_request);
+
+            // Assert
+
+            /// Verify that the Save method was called once
+            _ticketBookingRepositoryMock.Verify(x => x.Save(It.IsAny<TicketBooking>()), Times.Once);
+
+            Assert.NotNull(savedTicketBooking);
+            Assert.Equal(_request.FirstName, savedTicketBooking.FirstName);
+            Assert.Equal(_request.LastName, savedTicketBooking.LastName);
+            Assert.Equal(_request.Email, savedTicketBooking.Email);
+        }   
+    }
+}
---
+++ TicketBookingCore/TicketBookingRequestProcessor.cs
@@ -0,0 +1,41 @@
+﻿
+namespace TicketBookingCore
+{
+    public class TicketBookingRequestProcessor
+    {
+        //suggesting that _ticketBookingRepository will hold a reference to an object that implements this interface.
+        private readonly ITicketBookingRepository _ticketBookingRepository;
+        public TicketBookingRequestProcessor(ITicketBookingRepository ticketBookingRepository)
+        {
+            _ticketBookingRepository = ticketBookingRepository;
+        }
+
+        public TicketBookingResponse Book(TicketBookingRequest request)
+        {
+            if (request is null)
+            {
+                throw new ArgumentNullException(nameof(request));
+            }
+
+            _ticketBookingRepository.Save(Create<TicketBooking>(request));
+
+            return Create<TicketBookingResponse>(request);
+        }
+        /// <summary>
+        /// This method creates a new instance of the specified type 
+        /// and sets the properties from the request object.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        private static T Create<T>(TicketBookingRequest request) where T : TicketBookingBase, new()
+        {
+            return new T
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email
+            };
+        }
+    }
+}
